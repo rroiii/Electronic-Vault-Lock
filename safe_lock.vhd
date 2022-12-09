6e92a078aa2fc2fbf2eedb0b03b12a747df50f0c
Cattyman <88530324+cattyman919@users.noreply.github.com>
2022-12-09 16:29:56 +0700

    Fixed correct combination

--- safe_lock.vhd
+++ safe_lock.vhd
@@ -15,10 +15,17 @@ ENTITY safe_lock IS
 END safe_lock;
 
 ARCHITECTURE comb_lock OF safe_lock IS
-    TYPE state_type IS (start, digit1, digit2, digit3, digit4, unlocked, waitTimer); -- Define states for the combination lock
-    SIGNAL state : state_type; -- State of the combination lock
-    SIGNAL combination : STD_LOGIC_VECTOR(3 DOWNTO 0); -- Correct combination
-    SIGNAL counter : INTEGER RANGE 0 TO 300000; -- 5-minute counters
+    TYPE state_type IS (start, digit1, digit2, digit3, unlocked, waitTimer); -- Define states for the combination lock
+    SIGNAL state : state_type := start; -- State of the combination lock
+
+    TYPE int_array IS ARRAY(0 TO 3) OF INTEGER;
+    SIGNAL correctCombination : int_array := (4, 2, 3, 5);
+    SIGNAL digitLength : STD_LOGIC_VECTOR(3 DOWNTO 0);
+    SIGNAL correctD1, correctD2, correctD3, correctD4 : STD_LOGIC_VECTOR(3 DOWNTO 0);
+
+    --    SIGNAL combination : STD_LOGIC_VECTOR(3 DOWNTO 0); -- Correct combination
+
+    SIGNAL counter : INTEGER RANGE 0 TO 300000 := 0; -- 5-minute counters
 
     -- password encrypter component
     COMPONENT password_decrypter
@@ -33,12 +40,15 @@ ARCHITECTURE comb_lock OF safe_lock IS
     SIGNAL decrypted_password : STD_LOGIC_VECTOR (11 DOWNTO 0);
 BEGIN
     P1 : password_decrypter PORT MAP(encrypted_password => encrypted_password, decrypted_password => decrypted_password);
+    correctD1 <= STD_LOGIC_VECTOR(to_unsigned(correctCombination(0), digitLength'length));
+    correctD2 <= STD_LOGIC_VECTOR(to_unsigned(correctCombination(1), digitLength'length));
+    correctD3 <= STD_LOGIC_VECTOR(to_unsigned(correctCombination(2), digitLength'length));
+    correctD4 <= STD_LOGIC_VECTOR(to_unsigned(correctCombination(3), digitLength'length));
 
     PROCESS (clk, rst)
     BEGIN
         IF rst = '1' THEN
             state <= start; -- Reset the state to start
-            combination <= "0000"; -- Set the combination to 0000
             counter <= 0; -- Reset the counter to 0
             seg_min <= "0000"; -- Clear the minutes on the seven segment display
             seg_sec <= "0000"; -- Clear the seconds on the seven segment display
@@ -46,62 +56,53 @@ BEGIN
             CASE state IS
                 WHEN start =>
                     IF counter = 0 THEN -- If the delay has expired
-                        IF d = "0000" THEN -- First digit of combination
+                        seg_min <= "0000"; -- Clear the minutes on the seven segment display
+                        seg_sec <= "0000"; -- Clear the seconds on the seven segment display
+                        IF d = correctD1 THEN -- First digit of combination
                             state <= digit1;
-                            combination <= "0XXX"; -- Set next digit to ?
-                            seg_min <= "0000"; -- Clear the minutes on the seven segment display
-                            seg_sec <= "0000"; -- Clear the seconds on the seven segment display
                         ELSE
-                            state <= start; -- Incorrect first digit, reset to start state
+                            state <= waitTimer; -- Incorrect first digit, go to wait state
+                            counter <= 30000; -- Set the counter to 5 minutes
+                            seg_min <= STD_LOGIC_VECTOR(to_unsigned(counter / 6000, 4)); -- Calculate and display the minutes on the seven segment display
+                            seg_sec <= STD_LOGIC_VECTOR(to_unsigned(counter MOD 6000, 4)); -- Calculate and display the seconds on the seven segment display
                         END IF;
-                    ELSE
-                        counter <= counter - 1; -- Decrement the counter
-                        seg_min <= STD_LOGIC_VECTOR(to_unsigned(counter / 6000, 4)); -- Calculate and display the minutes on the seven segment display
-                        seg_sec <= STD_LOGIC_VECTOR(to_unsigned(counter MOD 6000, 4)); -- Calculate and display the seconds on the seven segment display
                     END IF;
                 WHEN digit1 =>
-                    IF d = "0XXX" THEN -- Second digit of combination
+                    IF d = correctD2 THEN -- Second digit of combination
                         state <= digit2;
-                        combination <= "0XX0"; -- Set next digit to 0
                         seg_min <= "0000"; -- Clear the minutes on the seven segment display
                         seg_sec <= "0000";
+                    ELSE
                         state <= waitTimer; -- Incorrect second digit, go to wait state
-                        counter <= 300000; -- Set the counter to 5 minutes
+                        counter <= 30000; -- Set the counter to 5 minutes
                         seg_min <= STD_LOGIC_VECTOR(to_unsigned(counter / 6000, 4)); -- Calculate and display the minutes on the seven segment display
                         seg_sec <= STD_LOGIC_VECTOR(to_unsigned(counter MOD 6000, 4)); -- Calculate and display the seconds on the seven segment display
                     END IF;
                 WHEN digit2 =>
-                    IF d = "0XX0" THEN -- Third digit of combination
+                    IF d = correctD3 THEN -- Third digit of combination
                         state <= digit3;
-                        combination <= "0X00"; -- Set next digit to 0
                         seg_min <= "0000"; -- Clear the minutes on the seven segment display
                         seg_sec <= "0000"; -- Clear the seconds on the seven segment display
                     ELSE
                         state <= waitTimer; -- Incorrect third digit, go to wait state
-                        counter <= 300000; -- Set the counter to 5 minutes
+                        counter <= 30000; -- Set the counter to 5 minutes
                         seg_min <= STD_LOGIC_VECTOR(to_unsigned(counter / 6000, 4)); -- Calculate and display the minutes on the seven segment display
                         seg_sec <= STD_LOGIC_VECTOR(to_unsigned(counter MOD 6000, 4)); -- Calculate and display the seconds on the seven segment display
                     END IF;
                 WHEN digit3 =>
-                    IF d = "0X00" THEN -- Fourth digit of combination
-                        state <= digit4;
+                    IF d = correctD4 THEN -- Fourth digit of combination
+                        state <= unlocked;
                         correct <= '1'; -- Correct combination has been entered
                         done <= '1'; -- Combination has been entered
                         seg_min <= "0000"; -- Clear the minutes on the seven segment display
                         seg_sec <= "0000"; -- Clear the seconds on the seven segment display
                     ELSE
                         state <= waitTimer; -- Incorrect fourth digit, go to wait state
-                        counter <= 300000; -- Set the counter to 5 minutes
+                        counter <= 30000; -- Set the counter to 5 minutes
                         seg_min <= STD_LOGIC_VECTOR(to_unsigned(counter / 6000, 4)); -- Calculate and display the minutes on the seven segment display
                         seg_sec <= STD_LOGIC_VECTOR(to_unsigned(counter MOD 6000, 4)); -- Calculate and display the seconds on the seven segment display
                     END IF;
-                WHEN digit4 =>
-                    IF d /= "0X00" THEN -- If the combination is not entered again
-                        state <= waitTimer; -- Go to wait state
-                        counter <= 300000; -- Set the counter to 5 minutes
-                        seg_min <= STD_LOGIC_VECTOR(to_unsigned(counter / 6000, 4)); -- Calculate and display the minutes on the seven segment display
-                        seg_sec <= STD_LOGIC_VECTOR(to_unsigned(counter MOD 6000, 4)); -- Calculate and display the seconds on the seven segment display
-                    END IF;
+
                 WHEN waitTimer =>
                     IF counter = 0 THEN -- If the delay has expired
                         state <= start; -- Go back to start state
@@ -111,18 +112,10 @@ BEGIN
                         counter <= counter - 1; -- Decrement the counter
                         seg_min <= STD_LOGIC_VECTOR(to_unsigned(counter / 6000, 4)); -- Calculate and display the minutes on the seven segment display
                         seg_sec <= STD_LOGIC_VECTOR(to_unsigned(counter MOD 6000, 4));
-                        state <= unlocked; -- Unlock the combination lock
-                        seg_min <= "0000"; -- Clear the minutes on the seven segment display
-                        seg_sec <= "0000"; -- Clear the seconds on the seven segment display
+
                     END IF;
                 WHEN unlocked =>
-                    IF d /= "0X00" THEN -- If the combination is not entered again
-                        state <= start; -- Reset to start state
-                        correct <= '0'; -- Incorrect combination
-                        done <= '0'; -- Combination has not been entered
-                        seg_min <= "0000"; -- Clear the minutes on the seven segment display
-                        seg_sec <= "0000"; -- Clear the seconds on the seven segment display
-                    END IF;
+
             END CASE;
         END IF;
     END PROCESS;
