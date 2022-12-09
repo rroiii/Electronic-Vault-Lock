eee683c7d57624b4355a369cc5a7f06b3e96cc16
Roy Oswaldha <royoswaldha@gmail.com>
2022-12-09 14:46:53 +0700

    Update safe_lock.vhdl

--- safe_lock.vhdl
+++ safe_lock.vhdl
@@ -1,120 +1,114 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity safe_lock is
     port (
-        user_input : in integer;   -- the input from the user
-        clk : in std_logic;        -- clock input
-        reset : in std_logic;      -- reset input
-        unlock : out std_logic     -- unlock output that indicates whether the lock is unlocked
+        clk: in std_logic;     -- Clock signal
+        rst: in std_logic;     -- Reset signal
+        d: in std_logic_vector(3 downto 0);     -- Input for the combination lock
+        correct: out std_logic;     -- Output indicating if the combination is correct
+        done: out std_logic;     -- Output indicating if the combination has been entered
+        seg_min: out std_logic_vector(3 downto 0);     -- Output for the minutes on the seven segment display
+        seg_sec: out std_logic_vector(3 downto 0)     -- Output for the seconds on the seven segment display
     );
-end entity safe_lock;
+end safe_lock;
 
-architecture rtl of safe_lock is
-    -- Define the states for the combination lock state machine
-    type state_type is (start, one, two, three, four, unlocked, waitTimer);
-    -- Define the current state of the state machine
-    signal current_state, next_state : state_type;
-
-    -- Define the next state of the state machine, based on the
-    -- current state and the input from the user
-    signal next_state : state_type;
-
-    -- Define the timer that is used to prevent the user from
-    -- entering the password again for 5 minutes
-    signal timer : time;
-
-    -- password encrypter component
-    component password_decrypter
-        Port ( 
-            encrypted_digit1 : in integer;
-            encrypted_digit2 : in integer;
-            encrypted_digit3 : in integer;
-            encrypted_digit4 : in integer;
-            decrypted_digit1 : in integer;
-            decrypted_digit2 : in integer;
-            decrypted_digit3 : in integer;
-            decrypted_digit4 : in integer;
-            );
-    end component;
-
-    -- password encrypter component instance
-    signal encrypted_password : STD_LOGIC_VECTOR (11 downto 0) := "111000100101";
-    signal decrypted_password : STD_LOGIC_VECTOR (11 downto 0);
-
+architecture comb_lock of safe_lock is
+    type state_type is (start, digit1, digit2, digit3, digit4, unlocked, wait);     -- Define states for the combination lock
+    signal state: state_type;     -- State of the combination lock
+    signal combination: std_logic_vector(3 downto 0);     -- Correct combination
+    signal counter: integer range 0 to 300000;     -- 5-minute counter
 begin
-    P1: password_decrypter Port map (encrypted_password=> encrypted_password, decrypted_password => encrypted_password);
-    -- The state machine that implements the combination lock
-    process(current_state, user_input, timer)
+    process(clk, rst)
     begin
-        -- Set the next state based on the current state and user input
-        case current_state is
+        if rst = '1' then
+            state <= start;     -- Reset the state to start
+            combination <= "0000";     -- Set the combination to 0000
+            counter <= 0;     -- Reset the counter to 0
+            seg_min <= "0000";     -- Clear the minutes on the seven segment display
+            seg_sec <= "0000";     -- Clear the seconds on the seven segment display
+        elsif rising_edge(clk) then
+            case state is
                 when start =>
-                    -- If the user enters 1, transition to state "one"
-                    if (digit_1 = encrypted_password(3 downto 0)) then
-                        next_state <= one;
+                    if counter = 0 then     -- If the delay has expired
+                        if d = "0000" then     -- First digit of combination
+                            state <= digit1;
+                            combination <= "0???;     -- Set next digit to ?
+                            seg_min <= "0000";     -- Clear the minutes on the seven segment display
+                            seg_sec <= "0000";     -- Clear the seconds on the seven segment display
+                        else
+                            state <= start;     -- Incorrect first digit, reset to start state
+                        end if;
                     else
-                        next_state <= start;
+                        counter <= counter - 1;     -- Decrement the counter
+                        seg_min <= to_unsigned(counter / 6000, 4);     -- Calculate and display the minutes on the seven segment display
+                        seg_sec <= to_unsigned(counter mod 6000, 4);     -- Calculate and display the seconds on the seven segment display
                     end if;
-
-                when one =>
-                    -- If the user enters 2, transition to state "two"
-                    if (digit_2 = encrypted_password(7 downto 4)) then
-                        next_state <= two;
+                when digit1 =>
+                    if d = "0???" then     -- Second digit of combination
+                        state <= digit2;
+                        combination <= "0??0";     -- Set next digit to 0
+                        seg_min <= "0000";     -- Clear the minutes on the seven segment display
+                        seg_sec <= "0000";    
+                        state <= wait;     -- Incorrect second digit, go to wait state
+                        counter <= 300000;     -- Set the counter to 5 minutes
+                        seg_min <= to_unsigned(counter / 6000, 4);     -- Calculate and display the minutes on the seven segment display
+                        seg_sec <= to_unsigned(counter mod 6000, 4);     -- Calculate and display the seconds on the seven segment display
+                    end if;
+                when digit2 =>
+                    if d = "0??0" then     -- Third digit of combination
+                        state <= digit3;
+                        combination <= "0?00";     -- Set next digit to 0
+                        seg_min <= "0000";     -- Clear the minutes on the seven segment display
+                        seg_sec <= "0000";     -- Clear the seconds on the seven segment display
                     else
-                        next_state <= start;
+                        state <= wait;     -- Incorrect third digit, go to wait state
+                        counter <= 300000;     -- Set the counter to 5 minutes
+                        seg_min <= to_unsigned(counter / 6000, 4);     -- Calculate and display the minutes on the seven segment display
+                        seg_sec <= to_unsigned(counter mod 6000, 4);     -- Calculate and display the seconds on the seven segment display
                     end if;
-
-                when two =>
-                    -- If the user enters 3, transition to state "three"
-                    if (digit_3 = encrypted_password(11 downto 8)) then
-                        next_state <= three;
+                when digit3 =>
+                    if d = "0?00" then     -- Fourth digit of combination
+                        state <= digit4;
+                        correct <= '1';     -- Correct combination has been entered
+                        done <= '1';     -- Combination has been entered
+                        seg_min <= "0000";     -- Clear the minutes on the seven segment display
+                        seg_sec <= "0000";     -- Clear the seconds on the seven segment display
                     else
-                        next_state <= start;
+                        state <= wait;     -- Incorrect fourth digit, go to wait state
+                        counter <= 300000;     -- Set the counter to 5 minutes
+                        seg_min <= to_unsigned(counter / 6000, 4);     -- Calculate and display the minutes on the seven segment display
+                        seg_sec <= to_unsigned(counter mod 6000, 4);     -- Calculate and display the seconds on the seven segment display
                     end if;
-
-                when three =>
-                    -- If the user enters 4, transition to state "four"
-                    if (digit_3 = encrypted_password(11 downto 8)) then
-                        next_state <= four;
-                    else
-                        next_state <= start;
+                when digit4 =>
+                    if d /= "0?00" then     -- If the combination is not entered again
+                        state <= wait;     -- Go to wait state
+                        counter <= 300000;     -- Set the counter to 5 minutes
+                        seg_min <= to_unsigned(counter / 6000, 4);     -- Calculate and display the minutes on the seven segment display
+                        seg_sec <= to_unsigned(counter mod 6000, 4);     -- Calculate and display the seconds on the seven segment display
                     end if;
-
-                when four =>
-                    -- If the user has entered the correct sequence, transition to the
-                    -- "unlocked" state
-                    next_state <= unlocked;
-
-                when unlocked =>
-                    -- If the lock is unlocked, stay in the "unlocked" state
-                    next_state <= unlocked;
-
                 when wait =>
-                    -- If the timer has expired, transition back to the "start" state
-                    if (timer = 0) then
-                        next_state <= start;
+                    if counter = 0 then     -- If the delay has expired
+                        state <= start;     -- Go back to start state
+                        seg_min <= "0000";     -- Clear the minutes on the seven segment display
+                        seg_sec <= "0000";     -- Clear the seconds on the seven segment display
                     else
-                        next_state <= waitTimer;
+                        counter <= counter - 1;     -- Decrement the counter
+                        seg_min <= to_unsigned(counter / 6000, 4);     -- Calculate and display the minutes on the seven segment display
+                        seg_sec <= to_unsigned(counter mod 6000, 4);
+                        state <= unlocked;     -- Unlock the combination lock
+                        seg_min <= "0000";     -- Clear the minutes on the seven segment display
+                        seg_sec <= "0000";     -- Clear the seconds on the seven segment display
                     end if;
-        end case;
-    end process;
-
-    -- Update the current state based on the next state
-    process(next_state)
-    begin
-        current_state <= next_state;
-
-        -- If the user has entered the incorrect password, start the timer
-        -- and transition to the "wait" state
-        if (current_state = start and next_state = start) then
-            timer <= 5 minutes;
-            next_state <= waitTimer;
+                when unlocked =>
+                    if d /= "0?00" then     -- If the combination is not entered again
+                        state <= start;     -- Reset to start state
+                        correct <= '0';     -- Incorrect combination
+                        done <= '0';     -- Combination has not been entered
+                        seg_min <= "0000";     -- Clear the minutes on the seven segment display
+                        seg_sec <= "0000";     -- Clear the seconds on the seven segment display
+                    end if;
+            end case;
         end if;
     end process;
-
-    
-    
-    
-end architecture rtl;
+end comb_lock;
