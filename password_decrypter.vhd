acf813dfd2536040b0e1813c5d036c28ba228700
Roy Oswaldha <royoswaldha@gmail.com>
2022-12-09 19:19:31 +0700

    Made password decryptor

--- password_decrypter.vhd
+++ password_decrypter.vhd
@@ -10,7 +10,14 @@ ENTITY password_decrypter IS
 END ENTITY password_decrypter;
 
 ARCHITECTURE rtl OF password_decrypter IS
-
+    signal temp : STD_LOGIC_VECTOR(0 to 15);
 BEGIN
+    for i in encrypted_password'range generate
+        temp(i) <= encrypted_password(i)  
+    end generate;
 
+    for i in encrypted_password'range generate
+        decrypted_password(i) <= encrypted_password(i) xor temp(i);
+    end generate;
+    
 END ARCHITECTURE rtl;
