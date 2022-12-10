cc6714f10ca71448f3093e88bf0794eb64d92d14
Sulsyd <88534639+Sulsyd@users.noreply.github.com>
2022-12-10 21:34:13 +0700

    Update lock_types.vhd

--- lock_types.vhd
+++ lock_types.vhd
@@ -2,8 +2,11 @@ LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 USE IEEE.numeric_std.ALL;
 
+
 PACKAGE lock_types IS
+
     TYPE state_digit IS (start, digit1, digit2, digit3, unlocked, waitTimer); -- Define states for the combination lock
-    TYPE state_lock IS (unlocking, setNewLock);
+    TYPE state_lock IS (unlocking, setNewLock); -- Defines the state of locks
     TYPE int_array IS ARRAY(0 TO 3) OF INTEGER;
+
 END PACKAGE lock_types;
